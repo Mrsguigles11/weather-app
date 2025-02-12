b68f51a4b6bf1e4fc73f6925bfb69712900b65c8
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-02-12 13:00:30 +0000

    Edit the fetchData function to return an object with only the relevant data.

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 
 async function fetchData(location) {
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?key=2YN9GZYD2E6P3FS56XRD9ENCT');
-    const data = response.json();
+    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?key=2YN9GZYD2E6P3FS56XRD9ENCT', {mode: 'cors'});
+    const json = await response.json();
+    const data = {
+        location: json.address,
+        description: json.description,
+        latitude: json.latitude,
+        longitude: json.longitude
+    }
     console.log(data);
-    return data;
 }
 
 export {fetchData};
